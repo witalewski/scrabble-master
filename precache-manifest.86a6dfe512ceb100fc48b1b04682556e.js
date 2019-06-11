1082799f73792a8b92c21910642a421106c6c86d
Krzysztof Adam Witalewski <krzysztof.witalewski@gmail.com>
2019-06-11 13:17:50 +0200

    Updates

--- precache-manifest.86a6dfe512ceb100fc48b1b04682556e.js
+++ precache-manifest.86a6dfe512ceb100fc48b1b04682556e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0ef1287158ce7955aa6ba42562f3f754",
+    "revision": "338340373b19157858db3e64ae167407",
     "url": "/scrabble-master/index.html"
   },
   {
-    "revision": "d145146c241f9a958775",
+    "revision": "b71dff38ca5f6886d220",
     "url": "/scrabble-master/static/css/main.5977cf58.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/scrabble-master/static/js/2.c1f626a9.chunk.js"
   },
   {
-    "revision": "d145146c241f9a958775",
-    "url": "/scrabble-master/static/js/main.28bb2367.chunk.js"
+    "revision": "b71dff38ca5f6886d220",
+    "url": "/scrabble-master/static/js/main.9d3fef19.chunk.js"
   },
   {
     "revision": "cc0877bd40b0ebc75ce0",
