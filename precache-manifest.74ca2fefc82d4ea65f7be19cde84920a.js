0908a262ca97f6c216a1f354c74614e597becb48
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-11 13:15:27 +0000

    Deploy witalewski/scrabble-master to github.com/witalewski/scrabble-master.git:gh-pages

--- precache-manifest.74ca2fefc82d4ea65f7be19cde84920a.js
+++ precache-manifest.74ca2fefc82d4ea65f7be19cde84920a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0ef1287158ce7955aa6ba42562f3f754",
+    "revision": "03e5950d72cdfafc642b73cf2474bb75",
     "url": "/scrabble-master/index.html"
   },
   {
-    "revision": "d145146c241f9a958775",
+    "revision": "ba5f82132082a58d584e",
     "url": "/scrabble-master/static/css/main.5977cf58.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/scrabble-master/static/js/2.c1f626a9.chunk.js"
   },
   {
-    "revision": "d145146c241f9a958775",
-    "url": "/scrabble-master/static/js/main.28bb2367.chunk.js"
+    "revision": "ba5f82132082a58d584e",
+    "url": "/scrabble-master/static/js/main.4cc43bab.chunk.js"
   },
   {
     "revision": "cc0877bd40b0ebc75ce0",
