ae8e1187f58af35320dbeb4eb658b462bb6f120d
Krzysztof Adam Witalewski <krzysztof.witalewski@gmail.com>
2019-06-11 13:14:22 +0200

    Updates

---
+++ precache-manifest.c8b2dd89520ffbf711002b1a098c2062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2dc610d5e85e2e9ff2c08dcfc3564ef",
+    "url": "/scrabble-master/index.html"
+  },
+  {
+    "revision": "698d39f8004edceee0ab",
+    "url": "/scrabble-master/static/css/main.5977cf58.chunk.css"
+  },
+  {
+    "revision": "6efdcb142ac443116baa",
+    "url": "/scrabble-master/static/js/2.c1f626a9.chunk.js"
+  },
+  {
+    "revision": "698d39f8004edceee0ab",
+    "url": "/scrabble-master/static/js/main.15b54b3a.chunk.js"
+  },
+  {
+    "revision": "cc0877bd40b0ebc75ce0",
+    "url": "/scrabble-master/static/js/runtime~main.0fdd47c6.js"
+  }
+]);
